94cd0c420e4643aa1883775b1c339f18212d8681
ilumer <ilumer.qxq@gmail.com>
2018-01-27 17:53:24 +0800

    完成badynames的练习

--- babynames/babynames.py
+++ babynames/babynames.py
@@ -1,3 +1,4 @@
+# coding:utf-8
 #!/usr/bin/python
 # Copyright 2010 Google Inc.
 # Licensed under the Apache License, Version 2.0
@@ -33,6 +34,8 @@ Suggested milestones for incremental development:
  -Build the [year, 'name rank', ... ] list and print it
  -Fix main() to use the extract_names list
 """
+def sort_by_alphabetical(tuple_name):
+  return tuple_name[0]
 
 def extract_names(filename):
   """
@@ -41,7 +44,27 @@ def extract_names(filename):
   ['2006', 'Aaliyah 91', Aaron 57', 'Abagail 895', ' ...]
   """
   # +++your code here+++
-  return
+  result = {};
+  with open(filename) as f:
+    text_content = f.read()
+    #year = re.search(r'<h3.*?>([\w\s]+?)(\d+)',text_content).group(2)
+    #2008 并没有使用<h3>标签
+    year = re.search(r'Popularity in (\d+)',text_content).group(1)
+    body_name_rank = re.findall(r'<td>(.+)</td><td>(.+)</td><td>(.+)</td>',text_content)
+    for item in body_name_rank:
+      #考虑有名字重合的情况
+      #if item[1] not in result.keys():
+      #  result[item[1]] = item[0]
+      #else
+      # value = result[item[1]]
+      # result[item[1]] = value if value < item[0] else item[0] 
+      result[item[1]] = item[0]
+      result[item[2]] = item[0]
+  name_list = [x+' '+y for x,y in result.items()]
+  name_list = sorted(name_list)
+  name_list.insert(0,year)
+  #print(name_list)
+  return name_list
 
 
 def main():
@@ -60,6 +83,11 @@ def main():
     summary = True
     del args[0]
 
+  for arg in args:
+    #写入文本中
+    with open(arg+'.summary','w') as f:
+      f.write('\n'.join(extract_names(arg)))
+    
   # +++your code here+++
   # For each filename, get the names, then either print the text output
   # or write it to a summary file
